8f754b7b620857157e1a9241ad02353b73e68d8a
Scarylie <saralie.bognandi@gmail.com>
2022-11-01 18:34:48 +0100

    completed

---
+++ src/reducers/products.js
@@ -0,0 +1,14 @@
+/* eslint-disable comma-dangle */
+import { createSlice } from '@reduxjs/toolkit';
+
+const productData = [
+  { id: 1, title: 'Socks', price: 100, emoji: '🧦' },
+  { id: 2, title: 'T-shirt', price: 200, emoji: '👕' },
+  { id: 3, title: 'Pants', price: 350, emoji: '👖' },
+  { id: 4, title: 'Dress', price: 550, emoji: '👗' },
+];
+
+export const products = createSlice({
+  name: 'producs',
+  initialState: productData,
+});
